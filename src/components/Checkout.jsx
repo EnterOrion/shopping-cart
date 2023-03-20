ef947402b7e1231d2c16aba5cb4a47d70856bedb
EnterOrion <orionwesson@gmail.com>
2023-03-20 09:34:59 +0530

    Basic cart page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import Navigation from "./Navigation";
-import ProductCard from "./ProductCard";
+import useItemStore from "../contexts/CartContext";
 
 const Checkout = () => {
+
+    const items = useItemStore((state) => state.itemsArray);
+    const listItems = items.map(
+        (element) => {
+            return (
+                <div className="checkout-item">
+                    <img className="checkout-image" src={element.image} alt="" /> 
+                    <div className="checkout-info">
+                        <div className="checkout-description">
+                            <div className="checkout-title"><strong>{element.name}</strong></div>
+                            <div>${element.price}</div>
+                            <div>Quantity: {element.quantity}</div>
+                            <div className="modify-quantity-buttons">
+                                <strong><span className="modify-quantity">-</span></strong>
+                                <strong><span className="modify-quantity">+</span></strong>
+                            </div>
+                        </div>
+                        <div className="remove-item">Delete</div>
+                    </div>
+                </div>
+            )
+        }
+
+    )
     return (
         
         <div>
@@ -11,7 +35,8 @@ const Checkout = () => {
                     <div className="checkout-and-title">
                     <h1 className="page-title">Shopping Cart</h1>
                     <div className="checkout-container">
-                    <p>Your cart is empty.</p>
+                    { listItems.length == 0 && <p>Your cart is empty!</p> }
+                    {listItems}
                     </div>
                     </div>
                 </div>
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -2,22 +2,48 @@ import React from "react";
 import {useParams} from "react-router-dom";
 import productRecords from "../assets/productRecords";
 import Navigation from "./Navigation";
+import { Link } from "react-router-dom";
+import useItemStore from "../contexts/CartContext";
 
 const ProductPage = () => {
      const {productId} = useParams();
-     console.log(productId);
      const thisProduct = productRecords.find(prod => prod.id == productId);
+     const items = useItemStore((state) => state.itemsArray);
+
+     const addItem = useItemStore((state) => state.addItem)
+
+     const addItemManager = () => {
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].name === thisProduct.name) {
+                console.log(true)
+                items[i].quantity = items[i].quantity + 1
+                return;
+            }
+        }
+        addItem(
+            {   image: thisProduct.img,
+                name: thisProduct.name, 
+                price: thisProduct.price,
+                quantity: 1
+                
+            });
+     }
+
      return (
         <div className="div">
             <Navigation />
             <div className="product-detail-page">
                 <div className="product-detail">
-                    <h1 className="detail-title">{thisProduct.name}</h1>
+                    <div className="button-header">
+                        <button className="back-button"><Link to="/shop">Go back</Link></button>
+                        <h1 className="detail-title">{thisProduct.name}</h1>
+                    </div>
                     <img className="detail-image" src={thisProduct.img} alt="" />
                     <div className="detail-info">
                         <p className="detail-price"><strong>Price:</strong> ${thisProduct.price}</p>
                         <p className="detail-description"><strong>Description:</strong> {thisProduct.description}</p>
-                        <div className="button-container"><button className="checkout-button">Add to Cart</button></div>
+                        <div className="button-container"><button className="checkout-button" onClick={addItemManager}>Add to Cart</button></div>
                     </div>
                 </div>
         </div>
