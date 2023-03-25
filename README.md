4f5d75cfede23771e7d1afc3623248455c09031d
EnterOrion <orionwesson@gmail.com>
2023-03-25 09:00:29 +0530

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Shopping Cart
+
+## About
+This is a fake website with a Boards of Canada theme, who are a Scottish electronic band. 
+The website features a product page of six of their notable releases in vinyl which the user can add to their cart to "buy". 
+
+## Tools 
+This web application was made with React and SASS. React Router was used to make navigating to different pages easier and a more user friendly experience. Zustand was used to easily manage state. 
+
+## Demo 
+https://enterorion.github.io/shopping-cart/
+
+## Download
+```
+git clone https://github.com/EnterOrion/shopping-cart.git
+cd shopping-cart
+npm install
+npm run start
+```
+
+## Attributions 
+* Landing page image by Jim Hu. 
+* Background image for the rest of the site by Lucas Kapla on Unplash. 
+* Vinyl pictures from Turntable Lab. 
+* Album cover pictures from Wikipedia. 
+* Cart icon by Udea on Flaticon. 
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import {Routes, Route, Outlet } from "react-router-dom";
 import App from "./App";
 import Products from "./components/Products";
 import Checkout from "./components/Checkout";
 import ProductPage from "./components/ProductPage";
 import CheckoutComplete from "./components/CheckoutComplete";
+import { HashRouter } from "react-router-dom";
 
 const RouteSwitch = () => {
   const [items, setItems] = React.useState(1);
@@ -12,15 +13,16 @@ const RouteSwitch = () => {
   return (
     <div>
       <Outlet context={[items, setItems]} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/shop" element={<Products />} />
-          <Route path="/cart" element={<Checkout />} />
-          <Route path="/shop/:productId" element={<ProductPage />} />
-          <Route path="/checkout-complete" element={<CheckoutComplete />} />
-        </Routes>
-      </BrowserRouter>
+      
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/shop" element={<Products />} />
+            <Route path="/cart" element={<Checkout />} />
+            <Route path="/shop/:productId" element={<ProductPage />} />
+            <Route path="/checkout-complete" element={<CheckoutComplete />} />
+          </Routes>
+        </HashRouter>
     </div>
   );
 };
